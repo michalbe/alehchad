cae5905a5932caa626d137f1e8a43a93ef4e80fc
Michał Budzyński <michal@virtualdesign.pl>
2016-06-30 03:17:00 +0100

    :gun: animation (doens work?)

--- example/script.js
+++ example/script.js
@@ -3,39 +3,55 @@
 
 var slides = [
   {
-    title: 'Foo'
+    title: 'F'
   },
   {
-    title: 'Bar'
+    title: 'E'
   }
 ];
 
-var getCharPath = function(svg, char) {
-  return createPathFromGlyph(svg.querySelector('[unicode="' + char + '"]'));
+var svg;
+
+var getCharPath = function(char, index) {
+  return createPathFromGlyph(svg.querySelector('[unicode="' + char + '"]'), index, true);
 };
 
 var dPosition = 500;
-var position = -dPosition;
+var position = 0;
 
-var createPathFromGlyph = function(glyph) {
-  position += dPosition;
-
-  return glyph.outerHTML.replace(
+var createPathFromGlyph = function(glyph, index, animation) {
+  var path = glyph.outerHTML.replace(
     '<glyph',
     '<path transform="scale(0.05, -0.05) translate(' + position + ', -1200)"'
   );
+
+  if (animation) {
+    console.log('bef', path);
+    var destPath = svg.querySelector('[unicode="' + slides[1].title[index] + '"]');
+    var d = destPath.getAttribute('d');
+    path = path.replace('/>',
+    '>' +
+      '<animate dur="5s" repeatCount="indefinite" attributeName="d"' +
+      'values="' + d + '">' +
+    '</path>');
+    console.log('aft', path);
+  }
+  position += dPosition;
+
+  return path;
 };
 
-var glyph;
+
 $.get('sample-font.svg').then(function(data) {
-  glyph = data.querySelector('glyph');
+
+  svg = data;
 
   var body = $('body');
 
   var text = slides[0].title;
   var title = '';
-  Array.prototype.forEach.call(text, function(letter) {
-    title += getCharPath(data, letter);
+  Array.prototype.forEach.call(text, function(letter, index) {
+    title += getCharPath(letter, index);
   });
 
   var path = '<svg width="1300" height="1500">' + title + '</svg>';
